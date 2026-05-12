5c6b1568712f692877b14fbf055a0f28934541f1
OliverBarta <barta3738@gmail.com>
2026-05-12 15:26:20 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import Board from './board.jsx'
+import './App.css'
+
+
+function App() {
+
+  return (
+    <>
+      <h1>WORDLE</h1>
+      <Board />
+
+    </>
+  )
+}
+
+export default App
---
+++ src/board.jsx
@@ -0,0 +1,20 @@
+import './board.css'
+
+function Board() {
+    return (
+        <section>
+            <div className="board">
+                <div className="Row">
+                    <button type="button" className="Square">X</button>
+                    <button type="button" className="Square">X</button>
+                    <button type="button" className="Square">X</button>
+                    <button type="button" className="Square">X</button>
+                    <button type="button" className="Square">X</button>
+                </div>
+            
+            </div>
+        </section>
+    )
+}
+
+export default Board
