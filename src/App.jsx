11a1796ec8258fd9a830f71c94491c68cab3c298
OliverBarta <barta3738@gmail.com>
2026-05-12 19:19:22 -0400

    Progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,41 @@
-
+import { useEffect, useState } from 'react'
 import Board from './board.jsx'
 import './App.css'
 
 
+
 function App() {
+  const [currentGuess, setCurrentGuess] = useState('')//'' means initialize as string
+  const [previousGuesses, setPreviousGuesses] = useState([])//[] means initialize as array
+
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+        // console.log(currentGuess)
+        if (event.key.match(/^[a-zA-Z]$/) && currentGuess.length < 5) {
+          setCurrentGuess(currentGuess + event.key.toUpperCase())
+        }
+        if (event.key === 'Backspace') {
+          if (currentGuess.length == 1 || currentGuess.length == 0) {
+            setCurrentGuess('')
+          } else {
+            setCurrentGuess(currentGuess.slice(0, -1))
+          }
+        }
+        if (event.key === 'Enter' && currentGuess.length == 5) {
+          setPreviousGuesses([...previousGuesses,currentGuess])
+          // console.log("Prev g f: ", previousGuesses)
+          setCurrentGuess('')
+        }
+      }
+    window.addEventListener('keydown', handleKeyDown)
+    return () => window.removeEventListener('keydown', handleKeyDown)
+  }, [currentGuess, previousGuesses])
 
   return (
     <>
       <h1>WORDLE</h1>
-      <Board />
-
+      <button type="button" className='newWord'>New Word</button>
+      <Board currentGuess={currentGuess} previousGuesses={previousGuesses} />
     </>
   )
 }
--- src/board.jsx
+++ src/board.jsx
@@ -1,17 +1,33 @@
+
 import './board.css'
 
-function Board() {
+function Board({currentGuess, previousGuesses}) {
+    const answer = "HELLO"
+
     return (
         <section>
             <div className="board">
+                {previousGuesses.map((guess, i) => (
+                    <div key={i} className='Row'>
+                        {guess.split("").map((letter, j) => (
+                            <div key={j} className={
+                                answer[j] === letter ? "SquareGreen" :
+                                answer.includes(letter) ? "SquareYellow" :
+                                "Square"
+                            }
+                            >
+                            {letter}
+                            </div>
+                        ))}
+                    </div>
+                ))}
                 <div className="Row">
-                    <button type="button" className="Square">X</button>
-                    <button type="button" className="Square">X</button>
-                    <button type="button" className="Square">X</button>
-                    <button type="button" className="Square">X</button>
-                    <button type="button" className="Square">X</button>
+                    {[0,1,2,3,4].map(i => (
+                        <div key={i} className="Square">
+                        {currentGuess[i] || ''}
+                        </div>
+                    ))}
                 </div>
-            
             </div>
         </section>
     )
